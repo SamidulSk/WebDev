ef1ebf60a389ae431ccf1eca324a6c2339a0d1ee
SamidulSk <samidulsk@@>
2024-08-30 07:19:33 +0530

    js practice

---
+++ FRONTEND/JS/practice/nine.js
@@ -0,0 +1,14 @@
+let score="43a";
+
+console.log(typeof(score));
+
+let valueInString=Number(score)
+console.log(valueInString)
+console.log(typeof(valueInString));
+console.log(typeof(NaN))
+
+//
+let value=5454;
+let stringNumber=String(value)
+console.log(typeof(stringNumber))
+
